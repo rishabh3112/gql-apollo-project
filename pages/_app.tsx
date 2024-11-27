42ec87fa2513d83ced78a87ff73fa8eff1e12824
Rishabh Chawla <rishabh31121999@gmail.com>
2024-11-27 15:37:59 +0530

    chore: add apollo provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
